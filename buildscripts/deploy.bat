23ee32447d258ad2819dc215a67f3de31356a36d
ankurptl18 <ankurpatel_18@yahoo.com>
2020-06-08 16:26:57 +0530

    update deploy.bat

--- buildscripts/deploy.bat
+++ buildscripts/deploy.bat
@@ -5,8 +5,8 @@ set targetTag=%2
 set sourceDir=%3
 
 echo STARTED TRANSFORMING THE SOURCE PACKAGE
-java -jar pwcSourceTransform.jar master %originTag% %targetTag% %sourceDir%/
+java -jar %sourceDir%/%targetTag%/buildscripts/build_engine/pwcSourceTransform.jar master %originTag% %targetTag% %sourceDir%/
 echo ENDED TRANSFORMING THE SOURCE PACKAGE
 echo STARTED JAR DEPENDENCY RESOLUTION
-java -jar PWCJarDependencyResolution.jar %targetTag% %sourceDir%/
+java -jar %sourceDir%/%targetTag%/buildscripts/build_engine/PWCJarDependencyResolution.jar %targetTag% %sourceDir%/
 echo ENDED TRANSFORMING THE SOURCE PACKAGE
