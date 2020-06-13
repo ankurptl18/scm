d23182a87e2d1524533f0ec53511927abd5d3784
ankurptl18 <ankurpatel_18@yahoo.com>
2020-06-14 02:45:35 +0530

    update deploy

--- buildscripts/deploy.bat
+++ buildscripts/deploy.bat
@@ -5,8 +5,8 @@ set targetTag=%2
 set sourceDir=%3
 
 echo ############ SOURCE STRUCTURE TRANSFORMATION 		: STARTED ############
-java -jar %sourceDir%/%targetTag%/buildscripts/build_engine/tools/pwcSourceTransform.jar master %originTag% %targetTag% %sourceDir%/
+java -jar %sourceDir%/%targetTag%/test/buildscripts/build_engine/tools/pwcSourceTransform.jar master %originTag% %targetTag% %sourceDir%/
 echo ############ SOURCE STRUCTURE TRANSFORMATION 		: END     ############
 echo ############ PROCESS FOR JAR DEPENDENCY RESOLUTION	: STARTED ############
-java -jar %sourceDir%/%targetTag%/buildscripts/build_engine/tools/PWCJarDependencyResolution.jar %targetTag% %sourceDir%/
+java -jar %sourceDir%/%targetTag%/test/buildscripts/build_engine/tools/PWCJarDependencyResolution.jar %targetTag% %sourceDir%/
 echo ############ PROCESS FOR JAR DEPENDENCY RESOLUTION	: ENDED   ############
