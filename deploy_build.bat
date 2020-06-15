838c2d69ff417bfefa554dd86785ad47abffdf5b
ankurptl18 <ankurpatel_18@yahoo.com>
2020-06-16 02:33:12 +0530

    Update

--- deploy_build.bat
+++ deploy_build.bat
@@ -1,9 +1,9 @@
 @echo off
-REM setlocal enabledelayedexpansion
+setlocal enabledelayedexpansion
 
 echo **************************************************************
 echo ******************** SETTING UP PARAMETERS *******************
-echo[
+echo.
 set repoBranchName=%1
 echo Repository Brach Name	: %repoBranchName%
 set originTag=%2
@@ -14,8 +14,8 @@ set sourceDir=%4
 echo Source Directory	: %sourceDir%
 set currentDir=%cd%
 echo Current Directory	: %currentDir%
-set targetStructureDirName=%sourceDir%/pwc.%repoBranchName%.%targetTag%/server/enovia/web-inf/lib/
-echo[ 
+
+echo. 
 echo **************************************************************
 
 REM Source Directory validation
@@ -31,37 +31,49 @@ REM Validating if origin tag is provided
 if "%~2"=="" (
 	echo **************************************************************
 	echo ******************* FULL BUILD	: STARTS *********************
-	echo[
-	echo ############ PLEASE PROVIDE BELOW DETAILS #############
-	echo[
-	REM set /p repoUserName="Enter Repository User name : "
-	REM set /p repoPassword="Enter Repository Password  : "
-	echo[
-	echo ############ TAKING FILES FROM REPOSITORY #############
-	REM git clone --branch %targetTag% https://github.com/ankurptl18/scm.git %sourceDir%/%targetTag%/
-	echo ############ ALL FILES HAS BEEN CHECKED OUT ###########
-	echo[
-	echo ############ PACKAGE TRANSFORMATION : STARTS ###########
-	REM call %sourceDir%/%targetTag%/buildscripts/deploy.bat %originTag% %targetTag% %sourceDir%
-	echo ############ PACKAGE TRANSFORMATION : END ###########
 	
-	set targetStructureDirName=%sourceDir%/pwc.%repoBranchName%.%targetTag%/server/enovia/web-inf/lib/
+	echo.
+	echo ############ PLEASE PROVIDE BELOW DETAILS #############
+	echo.
+		set /p repoUserName="Enter Repository User name : "
+		set /p repoPassword="Enter Repository Password  : "
+	echo.
+	
+	echo ############ TAKING FILES FROM REPOSITORY #############
+		git clone --branch %targetTag% https://github.com/ankurptl18/scm.git %sourceDir%/%targetTag%/
+	
+	echo ############ ALL FILES HAS BEEN CHECKED OUT ###########
+	echo.
+	
+	set targetStructureDirName=%sourceDir%/pwc.%repoBranchName%.%targetTag%/
+	
+	echo ############ PACKAGE TRANSFORMATION : STARTS ###########
+		call %sourceDir%/%targetTag%/buildscripts/deploy.bat %originTag% %targetTag% %sourceDir% %targetStructureDirName%
+	echo ############ PACKAGE TRANSFORMATION : END ###########
 	
 	echo ******************* FULL BUILD	: ENDS ***********************
 	
 ) else (
 	echo **************************************************************
 	echo ********************* DELTA BUILD STARTS *********************
-	REM set /p repoUserName="Enter Repository User name : "
-	REM set /p repoPassword="Enter Repository Password  : "
+	
+		set /p repoUserName="Enter Repository User name : "
+		set /p repoPassword="Enter Repository Password  : "
+
+	echo.
+	echo ############ TAKING FILES FROM REPOSITORY #############
+	
 	REM git clone --branch %originTag% https://github.com/ankurptl18/scm.git %sourceDir%/%originTag%/
 	REM git clone --branch %targetTag% https://github.com/ankurptl18/scm.git %sourceDir%/%targetTag%/
 	
-	set targetStructureDirName=%sourceDir%/pwc.%repoBranchName%.%originTag%_%targetTag%/server/enovia/web-inf/lib/
+	echo ############ ALL FILES HAS BEEN CHECKED OUT ###########
+	echo.
+	
+	set targetStructureDirName=%sourceDir%/pwc.%repoBranchName%.%originTag%_%targetTag%/
+	echo %targetStructureDirName%
+	echo ############ BUILD PROCEDURE : STARTS 		 ###########   
+		call %sourceDir%/%targetTag%/buildscripts/build_engine/deploy.bat %originTag% %targetTag% %sourceDir% %targetStructureDirName%
+	echo ############ BUILD PROCEDURE : ENDS 		 ###########   
+		
 	echo **************************************************************
 )
-
-echo **************************************************************
-echo %sourceDir%/%targetTag%/buildscripts/
-echo **************************************************************
-call ant -buildfile %sourceDir%/%targetTag%/buildscripts/build.xml jar -DjarSrcCodeDir=%sourceDir%/%targetTag%/sourcecode/java -DjarDestDir=%targetStructureDirName%
